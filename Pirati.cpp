a3d3f59eda1537ef96cb325786fb7b66ccef6073
Nikola <nikola.popovski@student.um.si>
2025-04-22 23:53:54 +0200

    Arrays

--- Pirati.cpp
+++ Pirati.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <limits>
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -15,13 +17,17 @@ int main(int argc, char* argv[]) {
 
     int n, m, k;
 
-    if (fscanf_s(file, "%d %d %d", &n, &m, &k) != 3) {
+    if (fscanf_s(file, "%d %d %d", &n, &m, &k) != 3 || n < 4 || m > 10000000 || k < 1 || k > 10000) {
         std::cerr << "Error reading n, m, k from file." << std::endl;
         fclose(file);
         return 1;
     }
 
-	for (int i = 0; i < k; ++i) {
+    std::vector<int> result(k + 1, {std::numeric_limits<int>::max()});
+    std::vector<std::pair<int, int>> A(k + 1, { std::numeric_limits<int>::max(), std::numeric_limits<int>::max() });
+    std::vector<std::pair<int, int>> A_copy(k + 1, { std::numeric_limits<int>::max(), std::numeric_limits<int>::max() });
+
+	for (int i = 0; i < k; i++) {
 		int a, b, c;
 		if (fscanf_s(file, "%d %d %d", &a, &b, &c) != 3 || a > n || b > m) {
 			std::cerr << "Error reading a, b, c from file." << std::endl;
