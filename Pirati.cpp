f3b427d80772c21a52b1f67bc1a3c2acc52b70d8
Nikola <nikola.popovski@student.um.si>
2025-04-24 05:09:16 +0200

    Half working

--- Pirati.cpp
+++ Pirati.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 #include <limits>
 #include <tuple>
+#include <algorithm>
+#include <utility>
 
 void print_array(const std::vector<std::pair<int, int>>& arr) {
     for (const auto& elem : arr) {
-        std::cout << elem.first << " " << elem.second << " ";
+        std::cout << elem.first << " " << elem.second << std::endl;
     }
-    std::cout << std::endl;
 }
 void print_array(const std::vector<int>& arr) {
     for (const auto& elem : arr) {
@@ -19,10 +20,52 @@ void print_array(const std::vector<int>& arr) {
 void print_all_three(const std::vector<std::pair<int, int>>& arr1, const std::vector<std::pair<int, int>>& arr2, const std::vector<int>& arr3) {
     int i = 0;
     for (const auto& elem : arr1) {
-		std::cout << i + 1 << ": " << arr1[i].first << " " << arr1[i].second << ", " << arr2[i].first << " " << arr2[i].second << ", " << arr3[i] << std::endl;
+        std::cout << i + 1 << ": " << arr1[i].first << " " << arr1[i].second << ", " << arr2[i].first << " " << arr2[i].second << ", " << arr3[i] << std::endl;
         i++;
     }
     std::cout << std::endl;
+}
+
+
+std::pair<int, int> min_num_with_place(std::vector<int> islands, int &m, int &numOfIslandsInRow, bool leftToRight) {
+	std::pair<int, int> result = { std::numeric_limits<int>::max(), -1 }; // first is result, second is index
+	int visitedIslands, tmp;
+    if (leftToRight) {
+        for (int i = 1; i <= numOfIslandsInRow; i++) {
+            visitedIslands = 0, tmp = 0;
+            for (int j = 0; j < m; j++) {
+                if (j != 0) tmp++;
+                if (islands[j] > 0) {
+                    visitedIslands++;
+                    if (visitedIslands == i && result.first > islands[j] + tmp) {
+                        result.first = islands[j] + tmp;
+                        result.second = j;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    else {
+		for (int i = 1; i <= numOfIslandsInRow; i++) {
+			visitedIslands = 0, tmp = 0;
+			for (int j = m - 1; j >= 0; j--) {
+				if (j != m - 1) tmp++;
+				if (islands[j] > 0) {
+					visitedIslands++;
+					if (visitedIslands == i && result.first > islands[j] + tmp) {
+						result.first = islands[j] + tmp;
+						result.second = j;
+						break;
+					}
+				}
+			}
+		}
+    }
+	if (result.second == -1) {
+		result.first = 0;
+	}
+	return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -45,10 +88,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-	std::vector<int> result(k + 1, { std::numeric_limits<int>::max() });
-    std::vector<std::pair<int, int>> A(k + 1, { std::numeric_limits<int>::max(), std::numeric_limits<int>::max() });
-    A[0].first = 0;
+	std::vector<int> result(k + 1, std::numeric_limits<int>::max());
+    std::vector<std::pair<int, int>> A(k + 1, { std::numeric_limits<int>::max() / 2, std::numeric_limits<int>::max() / 2});
 
+    A[0].first = 0;
 	//std::vector<std::tuple<int, int, int>> islands(k);
     std::vector<std::vector<int>> islands(n, std::vector<int>(m, 0));
 	std::vector<int> numOfIslandsInRow(n, 0);
@@ -68,62 +111,62 @@ int main(int argc, char* argv[]) {
     std::vector<std::pair<int, int>> A_copy;
 	int cumulative = 0, calc = 0, currentIslandsViseted; // cumulative of amount of islands in current and all previous rows, calc is for storing calculations
 	result[0] = 0;
+    A_copy = A;
+    A[0].first = std::min(A_copy[0].first, A_copy[0].second + m - 1);
+    A[0].second = std::min(A_copy[0].second, A_copy[0].first + m - 1);
+	std::pair<int, int> left, right;
 	for (int i = 0; i < n; i++) { // za vsako vrstico
 		cumulative += numOfIslandsInRow[i];
 
 		A_copy = A;
 
-        A[0].first = std::min(A_copy[0].first, A_copy[0].second + m);
-        A[0].second = std::min(A_copy[0].second, A_copy[0].first + m);
+        std::cout << "Result before row " << i << std::endl;
+        print_array(result);
+        std::cout << "A before row " << i << std::endl;
+        print_array(A);
+        std::cout << std::endl;
 
 		for (int j = 1; j < cumulative + 1; j++) {
-			if (n == 0) {
-                if (j == cumulative) { // za zadnji element
+			if (i == 0) {
+                if (j == cumulative - 1) { // za zadnji element
+                    left = min_num_with_place(islands[i], m, numOfIslandsInRow[i], true);
 
+                    result[j] = std::min(result[j], A_copy[0].first + left.first);
+                    A[j].first = std::min(A_copy[j].first, A_copy[0].first + left.first + left.second);
+                    A[j].second = std::min(A_copy[j].second, A_copy[0].first + left.first + m - 1 - left.second);
                 }
                 else {
+					left = min_num_with_place(islands[i], m, numOfIslandsInRow[i], true);
 
-
-
-
-                    //???????????????????????????????????????????
-                    //TUKI OSTAU BUKI
-                    //???????????????????????????????????????????
-                    for (int l = 1; l < m; l++) {
-                        calc++;
-						if (islands[i][l] > 0) {
-                            currentIslandsViseted++;
-							calc += islands[i][l];
-							if (currentIslandsViseted > j) break;
-						}
-					}
-                    //???????????????????????????????????????????
-                    //???????????????????????????????????????????
-                    //???????????????????????????????????????????
-
-
-
-
-
-
-
-
-
-
-
-
-                    result[j] = std::min(result[j], A_copy[j - 1].first + calc, A_copy[j - 1].first + 99990000000000000000000000000000000);
-                    A[j].first = std::min(A_copy[j].first, A_copy[j - 1].first + 9999999999999999999999999999999999999, A_copy[j - 1].first + 99999999999999999999999999999999);
-                    A[j].second = std::min(A_copy[j].second, A_copy[j - 1].first + 9999999999999999999999999999999999999, A_copy[j - 1].first + 99999999999999999999999999999999);
+                    result[j] = std::min(result[j], A_copy[0].first + left.first);
+                    A[j].first = std::min(A_copy[j].first, A_copy[0].first + left.first + left.second);
+                    A[j].second = std::min(A_copy[j].second, A_copy[0].first + left.first + m - 1 - left.second);
                 }
             }
             else {
+                right = min_num_with_place(islands[i], m, numOfIslandsInRow[i], false);
+                left = min_num_with_place(islands[i], m, numOfIslandsInRow[i], true);
+                
 
+                if (left.second != -1) {
+                    result[j] = std::min({ result[j], A_copy[j - 1].first + left.first, A_copy[j - 1].second + right.first });
+                    A[j].first = std::min({ A_copy[j].first, A_copy[j - 1].first + left.first + left.second, A_copy[j - 1].second + right.first + right.second });
+                    A[j].second = std::min({ A_copy[j].second, A_copy[j - 1].first + left.first + m - 1 - left.second, A_copy[j - 1].second + right.first + m - 1 - right.second });
+                }
             }
 		}
+
+        for (int i = 0; i < k + 1; i++) {
+            if (A[i].first != std::numeric_limits<int>::max()) {
+                A[i].first++;
+            }
+            if (A[i].second != std::numeric_limits<int>::max()) {
+                A[i].second++;
+            }
+        }
 	}
 
-    print_all_three(A, A_copy, result);
+    print_array(result);
 
     fclose(file);
     return 0;
